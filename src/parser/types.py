3a6b5e0fe791206ad13cde13ffdd2c2c8b60ff1a
usrsem <andrey7menov@gmail.com>
2022-04-17 20:02:20 +0300

    Overriding __repr__ and __str__ for Direction Enum

--- src/parser/types.py
+++ src/parser/types.py
@@ -12,7 +12,13 @@ class Lang(Enum):
 
 
 class Direction(Enum):
-    LONG = 0
-    SHORT = 1
-    NOT_FOUND = 2
+    LONG = "long"
+    SHORT = "short"
+    NOT_FOUND = "empty"
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.__str__()
 
